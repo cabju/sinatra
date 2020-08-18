0696930c6a27c3eaf2405f4132deab17b7427d3f
Carlo <Carlo_junior@hotmail.co.uk>
2020-08-18 16:07:51 +0100

    views

--- app.rb
+++ app.rb
@@ -1,26 +1,5 @@
-require "sinatra"
-get '/' do
-    "Hello world"
-end
-
-get '/secret' do
-  "Today is Monday"
-end
-
-get '/carlo' do
-  "tomorrow is tuesday"
-end
-
-get '/iliyan' do
-  "my pairing parnter"
-end
-
-get '/coffee' do
-  "iliyan loves it"
-end
+require 'sinatra'
 
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-  <img src='http://bit.ly/1eze8aE'>
-  </div>"
+  erb(:index)
 end
